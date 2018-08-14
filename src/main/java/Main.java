7248ef03782086fd5447b9687ff58cbeb7252c0c
abr227 <christian.graumann@haw-hamburg.de>
2018-08-14 23:44:12 +0200

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,64 @@
+import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.multipdf.LayerUtility;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject;
+
+import java.awt.geom.AffineTransform;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author Chris on 31.07.2018
+ */
+public class Main {
+
+
+    private static final String SOURCE_DIRECTORY = "test.pdf";
+    private static final String TYPE = "se"; // se or gka
+
+    public static void main(String[] args) throws IOException {
+
+        PDFMaker maker = new PDFMaker();
+
+        File fileOrFolder = new File(SOURCE_DIRECTORY);
+
+        if(!fileOrFolder.isFile()){
+            File[] listOfFiles = fileOrFolder.listFiles();
+
+            for (int i = 0; i < listOfFiles.length; i++) {
+
+                if (listOfFiles[i].isFile() && getFileExtension(listOfFiles[i]).equals("pdf")) {
+                    System.out.println("File " + listOfFiles[i].getName());
+                    maker.makePDF(listOfFiles[i], TYPE);
+                }
+            }
+        } else {
+
+            if (getFileExtension(fileOrFolder).equals("pdf")) {
+                System.out.println("File " + fileOrFolder.getName());
+                maker.makePDF(fileOrFolder, TYPE);
+            }
+
+        }
+
+
+
+
+    }
+
+    private static String getFileExtension(File file) {
+        String fileName = file.getName();
+        if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+            return fileName.substring(fileName.lastIndexOf(".")+1);
+        else return "";
+    }
+
+
+
+
+}
---
+++ src/main/java/PDFMaker.java
@@ -0,0 +1,39 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Chris on 31.07.2018
+ */
+public class PDFMaker {
+
+    public void makePDF(File file, String type) throws IOException {
+
+        PDDocument target = new PDDocument();
+
+        PDDocument source = PDDocument.load(file);
+        PDDocument toAdd;
+
+        if(type.equals("se")){
+            toAdd = PDDocument.load(new File("se.pdf"));
+        } else if (type.equals("gka")){
+            toAdd = PDDocument.load(new File("gka.pdf"));
+        } else {
+            toAdd = PDDocument.load(new File("se.pdf"));
+        }
+
+        for(int i = 0; i < source.getNumberOfPages(); i++){
+            target.addPage(source.getPage(i));
+            target.addPage( toAdd.getPage(0));
+        }
+
+
+        target.save(file);
+        toAdd.close();
+        source.close();
+        target.close();
+
+    }
+    
+}
