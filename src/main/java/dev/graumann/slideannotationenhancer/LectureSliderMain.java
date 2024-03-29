342f00926c560eb7185b682ba2bb4e5419189d9c
cgraumann <christian.graumann@outlook.de>
2024-03-29 22:30:14 +0100

    - add style sheet - add mainModule

--- src/main/java/dev/graumann/slideannotationenhancer/LectureSliderMain.java
+++ src/main/java/dev/graumann/slideannotationenhancer/LectureSliderMain.java
@@ -13,6 +13,7 @@ public class LectureSliderMain extends Application {
 
     private static final String MAIN_VIEW_PATH = "/dev/graumann/slideannotationenhancer/fxml/main.fxml";
     private static final String STAGE_TITLE = "Slide Annotation Enhancer";
+    private static final String STYLE_CSS = "/dev/graumann/slideannotationenhancer/css/style.css";
 
     private static final String SVG_ICON_TITELBAR_LOGO = "M11.99 18.54l-7.37-5.73L3 14.07l9 7 9-7-1.63-1.27zM12 16l7.36-5.73L21 9l-9-7-9 7 1.63 1.27L12 16zm0-11.47L17.74 9 12 13.47 6.26 9 12 4.53z";
 
@@ -22,6 +23,10 @@ public class LectureSliderMain extends Application {
 
         FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource(MAIN_VIEW_PATH));
         Parent userContent = fxmlLoader.load();
+
+        // Style hier adden, in der main.fxml führt dazu, dass die .css nur bei run, aber nicht im jar gefunden wird
+        userContent.getStylesheets().clear();
+        userContent.getStylesheets().add(getClass().getResource(STYLE_CSS).toExternalForm());
 
         Rectangle2D bounds = Screen.getScreens().get(0).getBounds();
         double width = bounds.getWidth() / 2.5;
